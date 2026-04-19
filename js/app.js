ccde2c3ea1ea7c8cf61569e69c0fddbe58e076bc
alfrerey1308-sys <alfrerey1308@gmail.com>
2026-04-19 15:41:21 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function mostrarMensaje() {
+    alert("¡Bienvenido a Zacatlán, Pueblo Mágico!");
+}
+
+// Cambiar modo oscuro
+function cambiarTema() {
+    document.body.classList.toggle("modo-oscuro");
+}
